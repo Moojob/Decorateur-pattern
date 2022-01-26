6051190918154cf2093274f815253422243dbb01
Moojob <moudiop123@gmail.com>
2022-01-26 17:15:16 +0000

    decorateur pattern

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,7 @@ class SejourAvecOption implements ISejour{
         this.sejourDecore = _unSejourDecore
     }
     prix(): number{
-        return this.sejourDecore.prix()+100
+        return this.sejourDecore.prix()
     }
 }
 
@@ -24,5 +24,35 @@ class SejourAvecResto extends SejourAvecOption{
         return super.prix()+100
     }
 }
+
+class SejourAvecPiscine extends SejourAvecOption{
+    prix() : number{
+        return super.prix() + 50
+    }
+}
+
+class SejourAvecSport extends SejourAvecOption{
+    prix() : number{
+        return super.prix() + 150
+    }
+}
+
+
+
 const sejourSimple = new sejour();
 console.log("Prix du sejour simple : ", sejourSimple.prix());
+
+const sejour1 = new SejourAvecResto(sejourSimple);
+console.log("Prix du sejour avec Resto : ", sejour1.prix());
+
+const sejour2 = new SejourAvecPiscine(sejourSimple);
+console.log("Prix du sejour avec Piscine : ", sejour2.prix());
+
+const sejour3 = new SejourAvecResto(sejour2);
+console.log("Prix du sejour avec Piscine et Resto : ", sejour3.prix());
+
+const sejour4 = new SejourAvecPiscine(sejour1);
+console.log("Prix du sejour avec Resto et Piscine: ", sejour4.prix());
+
+const sejour5 = new SejourAvecSport(new SejourAvecResto(new sejour()));
+console.log("Prix du sejour avec Resto et Sport: ", sejour5.prix());
